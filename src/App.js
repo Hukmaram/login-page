84fde9b10ef01cdc6246215816bb64c37d073d83
Hukmaram-Bishnoi <engineerhukmaram29@gmail.com>
2022-04-27 15:04:24 +0530

    simple login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Checkbox,
+  Grid,
+  TextField,
+  FormControlLabel,
+  Paper,
+  Button,
+  Typography 
+} from '@material-ui/core';
+import { makeStyles,createStyles,styled } from "@material-ui/core/styles";
+const useStyles = makeStyles(() =>
+    createStyles({
+        gridHeight:{
+          height:'100vh'
+        },
+        logoText:{
+          marginLeft:'40vw'
+        }
+}));
 
 function App() {
+  const classes=useStyles()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Paper>
+        <Grid 
+        container
+        spacing={3}
+        direction={'row'}
+        justify={'center'}
+        alignItems={'center'}
+        className={classes.gridHeight}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Grid item xs={12} md={6}>
+          <Typography variant="h6" component="h6" className={classes.logoText}>
+           contents
+        </Typography>
+            </Grid>
+          <Grid item xs={12} md={6}>
+          <Grid
+          container
+          spacing={3}
+          direction={'column'}
+          justify={'center'}
+          alignItems={'center'}
+        >
+          <Grid item xs={12}>
+            <TextField label="Username"></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label="Password" type={'password'}></TextField>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  label={'Keep me logged in'}
+                  inputProps={{ 'aria-label': 'primary checkbox' }}
+                />
+              }
+              label="Keep me logged in"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button fullWidth> Login </Button>
+          </Grid>
+        </Grid>
+        </Grid>
+        </Grid>
+      </Paper>
   );
 }
 
